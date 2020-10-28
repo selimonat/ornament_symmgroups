f8a5c2acda2106d6046bbb753cc4f0da7d2b7b1b
onatselim <onatselim@gmail.com>
2020-10-28 10:21:42 +0100

    changed title

--- SimilaritySimulation_Run.m
+++ SimilaritySimulation_Run.m
@@ -9,7 +9,7 @@ SimilaritySimulation(group1 ,group2)
 subplot(nrow,ncol,1+ncol)
 SimilaritySimulation(group1 -mean(group1),group2-mean(group2),'MINUS MEAN')
 subplot(nrow,ncol,1+ncol*2)
-SimilaritySimulation(group1 - sum(group1),group2-sum(group2),'MINUS SUM')
+SimilaritySimulation(group1 /sum(group1),group2/sum(group2),'DIVIDE by SUM')
 
 subplot(nrow,ncol,2)
 group1 = [1,0];
@@ -18,7 +18,7 @@ SimilaritySimulation(group1 ,group2)
 subplot(nrow,ncol,2+ncol)
 SimilaritySimulation(group1 -mean(group1),group2-mean(group2),'MINUS MEAN')
 subplot(nrow,ncol,2+ncol*2)
-SimilaritySimulation(group1 - sum(group1),group2-sum(group2),'MINUS SUM')
+SimilaritySimulation(group1 /sum(group1),group2/sum(group2),'DIVIDE by SUM')
 
 subplot(nrow,ncol,3)
 group1 = [.6,.8];
@@ -27,7 +27,7 @@ SimilaritySimulation(group1 ,group2)
 subplot(nrow,ncol,3+ncol)
 SimilaritySimulation(group1 -mean(group1),group2-mean(group2),'MINUS MEAN')
 subplot(nrow,ncol,3+ncol*2)
-SimilaritySimulation(group1 - sum(group1),group2-sum(group2),'MINUS SUM')
+SimilaritySimulation(group1 /sum(group1),group2/sum(group2),'DIVIDE by SUM')
 
 subplot(nrow,ncol,4)
 group1 = [.6,.8]*3;
@@ -36,4 +36,4 @@ SimilaritySimulation(group1 ,group2)
 subplot(nrow,ncol,4+ncol)
 SimilaritySimulation(group1 -mean(group1),group2-mean(group2),'MINUS MEAN')
 subplot(nrow,ncol,4+ncol*2)
-SimilaritySimulation(group1 - sum(group1),group2-sum(group2),'MINUS SUM')
+SimilaritySimulation(group1 / sum(group1),group2/sum(group2),'DIVIDE by SUM')
