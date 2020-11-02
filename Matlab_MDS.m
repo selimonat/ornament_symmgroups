becf65175fe03d09bf9836e237236199e8a3f13a
onatselim <onatselim@gmail.com>
2020-11-02 16:50:07 +0100

    second iteration

--- Matlab_MDS.m
+++ Matlab_MDS.m
@@ -4,13 +4,14 @@ clf;
 % SET PARAMETERS FOR ANALYSIS
 % which metric to use for computing distances.
 % type help pdist to see other options.
-DISTANCE_METRIC = 'euclidean'%'correlation'
-% 
-MDS_TYPE = 'not_classical' % classical uses cmdscale function (classical mds), 
-% in order to use mdscale function change MDS_TYPE to something else.
-% read their documentation with help cmdscale or help mdscale
 WHICH_DATA = 'YUNAN' %(or YUNAN) 
 
+DISTANCE_METRIC = 'euclidean'%'correlation'
+% 
+MDS_TYPE = 'nonclassical' % classical uses cmdscale function (classical mds), 
+% in order to use mdscale function change MDS_TYPE to something else.
+% read their documentation with help cmdscale or help mdscale
+CRITERION  = 'metricstress'
 
 % READ DATA
 fprintf('importing data\n');
@@ -48,16 +49,20 @@ set(gca,'fontsize',16)
 yticks(1:length(headers));yticklabels(headers);
 xticks(1:length(headers));xticklabels(headers);xtickangle(45)
 title(sprintf(['Dissimilarity Matrix\nwith original data']),'fontsize',12)
-%
-if strcmp(MDS_TYPE,'classical')
-    fprintf('Using classical')
-    [Y,stress] = cmdscale(dissimilarities,4);
-else
-    [Y,stress] = mdscale(dissimilarities,4,'criterion','metricstress');
-end
-stress
+
 % How good we approximate the original dissimilarity matrix when using only
 % 1 dimension
+%
+DIMEN = 1
+if strcmp(MDS_TYPE,'classical')
+    fprintf('Using classical')
+    [Y,stress] = cmdscale(dissimilarities,DIMEN);
+elseif strcmp(MDS_TYPE,'nonclassical')
+    fprintf('Using non-classical metric')
+    [Y,stress] = mdscale(dissimilarities,DIMEN,'criterion',CRITERION);
+end
+stress
+
 maxerr2 = mean(abs(dissimilarities - pdist(Y(:,1),DISTANCE_METRIC)));
 figure(1);subplot(3,4,2);
 imagesc(squareform( pdist(Y(:,1),DISTANCE_METRIC)));colorbar;
@@ -72,6 +77,15 @@ xticks(1:length(headers));xticklabels(headers);xtickangle(45)
 
 % How good we approximate the original dissimilarity matrix when using only
 % 2 dimensions
+DIMEN = 2
+if strcmp(MDS_TYPE,'classical')
+    fprintf('Using classical')
+    [Y,stress] = cmdscale(dissimilarities,DIMEN );
+elseif strcmp(MDS_TYPE,'nonclassical')
+    fprintf('Using non-classical metric')
+    [Y,stress,disparities] = mdscale(dissimilarities,DIMEN,'criterion',CRITERION,'Replicates',100,'start','random','options',statset('Display','final','MaxIter',10000));
+end
+stress
 maxerr3 = mean(abs(dissimilarities - pdist(Y(:,1:2),DISTANCE_METRIC)));
 figure(1);subplot(3,4,6);
 imagesc(squareform( pdist(Y(:,1:2),DISTANCE_METRIC)));colorbar;
@@ -84,13 +98,29 @@ plot(Y(:,1),Y(:,2),'r+');title('MDS plot (2 dimen)');
 for i = [1:length(headers)]
     text(Y(i,1),Y(i,2),headers{i},'fontsize',16)
 end
-
+% 
 % How good we approximate the original dissimilarity matrix when using only
 % 3 dimension
+DIMEN = 3
+if strcmp(MDS_TYPE,'classical')
+    fprintf('Using classical')
+    [Y,stress] = cmdscale(dissimilarities,DIMEN );
+elseif strcmp(MDS_TYPE,'nonclassical')
+    fprintf('Using non-classical metric')
+    [Y,stress] = mdscale(dissimilarities,DIMEN,'criterion',CRITERION,'Replicates',10,'start','random','options',statset('Display','final','MaxIter',10000));
+end
+stress
+
 maxerr4 = mean(abs(dissimilarities - pdist(Y(:,1:3),DISTANCE_METRIC)));
 figure(1);subplot(3,4,10);
 imagesc(squareform( pdist(Y(:,1:3),DISTANCE_METRIC)));colorbar;
 title(sprintf(['Dissimilarity Matrix with 3 MDS dimensions \n mean(abs(Error)):' mat2str(maxerr4,3) ]));
 subplot(3,4,11);
 imagesc(squareform(abs(dissimilarities - pdist(Y(:,1:3),DISTANCE_METRIC))));colorbar;
+title(sprintf(['Reconstruction Error (dimen = 3)']));
+subplot(3,4,12);
+plot3(Y(:,1),Y(:,2),Y(:,3),'r+');title('MDS plot (3 dimen)');
+for i = [1:length(headers)]
+    text(Y(i,1),Y(i,2),Y(i,3),headers{i},'fontsize',16)
+end
 title(sprintf(['Reconstruction Error (dimen = 3)']));
