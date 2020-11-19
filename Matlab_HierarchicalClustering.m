3d79687b30cba7dd679903c86d73f49c38a308eb
onatselim <onatselim@gmail.com>
2020-11-19 11:22:40 +0100

    hierarchical clustering analysis

--- Matlab_HierarchicalClustering.m
+++ Matlab_HierarchicalClustering.m
@@ -1,11 +1,28 @@
 ORDER = [4,3,2,5,6,7];
 [data,headers,dummy] = xlsread('data_paper_Greek.xlsx');
 headers = {headers{1,2:end}};
-headers = headers(ORDER);
+headers = headers(ORDER)
+
 % discard useless last rows
 data = data(1:17,ORDER);   
 figure(2)
-Z = linkage(data','complete','euclidean');
-dendrogram(Z,'Labels', headers,'orientation', 'left')
+% OPTIONS:
+%       'single'    --- nearest distance (default)
+%      'complete'  --- furthest distance
+%      'average'   --- unweighted average distance (UPGMA) (also known as
+%                      group average)
+%      'weighted'  --- weighted average distance (WPGMA)
+%      'centroid'  --- unweighted center of mass distance (UPGMC)
+%      'median'    --- weighted center of mass distance (WPGMC)
+%      'ward'      --- inner squared distance (min variance algorithm)
+Z = linkage(data','average','euclidean')
+dendrogram(Z,'Labels', headers,'orientation', 'top');set(gca,'fontsize',20)
 
-set(gca,'fontsize',20)
+cophenet(Z,pdist(data','euclidean'))
+
+inconsistent(Z,3)
+
+c = cluster(Z,'cutoff',.4)
+
+% %      crosstab(c,species)
+%      dendrogram(Z)
